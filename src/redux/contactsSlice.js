711f6d5eb77beaf9a064be3dd861f40fb01a5a84
Alex <alexlipskyi@icloud.com>
2023-10-12 18:29:32 +0300

    toolkit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const contactsSlice = createSlice({
+
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+
+      prepare(values) {
+        return {
+          payload: {
+            id: nanoid(),
+            contact: values.name,
+            number: values.number,
+          },
+        };
+      },
+    },
+
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const {addContact,deleteContact} = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
